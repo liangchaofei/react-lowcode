57cf3832cd4682e784b6a514548197657df1f640
liangchaofei <chaofei_liang@126.com>
2023-11-30 11:41:17 +0800

    feat: 获取用户信息

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,13 +1,38 @@
 import React, { FC } from "react";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button, message } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import { useRequest } from 'ahooks'
 import { LOGIN_PATHNAME } from '../../router'
+import { removeToken } from '../../utils/user-token'
+import { getUserInfoService } from '../../services/user'
 
 const UserInfo: FC = () => { 
-    return (
-        <div>
-            <Link to={LOGIN_PATHNAME}>登录</Link>
-        </div>
+    const nav = useNavigate()
+    const { data } = useRequest(getUserInfoService) // ajax
+    const { username, nickname } = data || {}
+
+    function logout() {
+        removeToken() // 清除 token 的存储
+        message.success('退出成功')
+        nav(LOGIN_PATHNAME)
+    }
+    
+    const UserInfo = (
+        <>
+          <span style={{ color: '#e8e8e8' }}>
+            <UserOutlined />
+            {nickname}
+          </span>
+          <Button type="link" onClick={logout}>
+            退出
+          </Button>
+        </>
     )
+
+    const Login = <Link to={LOGIN_PATHNAME}>登录</Link>
+    
+    return <div>{username ? UserInfo : Login}</div>
 }
 
 export default UserInfo 
